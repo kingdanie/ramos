7bcfba97fd36c93f578bd59a8f0db94f3aaff91b
kingdanie <daniedemola@gmail.com>
2024-03-09 22:06:29 +0100

    imrpoved import on iconsa

--- app/component/icon.tsx
+++ app/component/icon.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 
-const Play = () => {
-  return (
-    <>
+const Play = () => (
     <svg fill="#fff" height="20px" width="20px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17.804 17.804" stroke="#fff">
         <g id="SVGRepo_bgCarrier" strokeWidth="0"/>
 
@@ -10,14 +8,10 @@ const Play = () => {
 
         <g id="SVGRepo_iconCarrier"> <g> <g id="c98_play"> <path d="M2.067,0.043C2.21-0.028,2.372-0.008,2.493,0.085l13.312,8.503c0.094,0.078,0.154,0.191,0.154,0.313 c0,0.12-0.061,0.237-0.154,0.314L2.492,17.717c-0.07,0.057-0.162,0.087-0.25,0.087l-0.176-0.04 c-0.136-0.065-0.222-0.207-0.222-0.361V0.402C1.844,0.25,1.93,0.107,2.067,0.043z"/> </g> <g id="Capa_1_78_"> </g> </g> </g>
     </svg>
-    </>
-  )
-}
+);
 
 
-const Bolt = () => {
-  return (
-    <>
+const Bolt = () => (
     <svg width="64px" height="64px" viewBox="0 0 15 15" fill="#fe4a23" xmlns="http://www.w3.org/2000/svg" stroke="#fe4a23">
 
         <g id="SVGRepo_bgCarrier" stroke-width="0"/>
@@ -29,14 +23,10 @@ const Bolt = () => {
             </g>
 
     </svg>
-    </>
-  )
-}
+);
 
 
-const Nodes = () => {
-  return (
-    <>
+const Nodes = () => (
         <svg width="64px" height="64px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#ffffff" stroke="#ffffff">
             <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
@@ -46,10 +36,8 @@ const Nodes = () => {
         </path>
         </g>
         </svg>   
-    </>
-  )
-}
+);
 
 
 
-export default {Play, Bolt, Nodes};
+export { Play, Bolt, Nodes };
